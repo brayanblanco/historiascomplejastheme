710c7ccd38dddb969b71c4ac4087c4bb085a4837
Aqueloutrado <brayan.martin.blanco@gmail.com>
2021-03-18 15:07:01 +0000

    slideshow buttons and temp script

--- home.php
+++ home.php
@@ -7,14 +7,54 @@
 ?>
 <section id="aboutme"></section>
 <section id="slideshow">
-  <h2 class="home-section-title">ÚLTIMAS ENTRADAS</h2>
+  <div class="slideshow-container">
+    <h2 class="home-section-title">ÚLTIMAS ENTRADAS</h2>
   <?php
-      for ($i = 1; $i <= 5; $i++ ){
+      $postscount = 0;
+
+      for ($i = 0; $i < 5; $i++ ){ //TODO: ideally the number of posts would be configurable
+        if(!have_posts())
+          break;
+
+        $postscount++;
         the_post();
 
         get_template_part( 'template-parts/content', 'home' );
       }
       ?>
+
+      <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+      <a class="next" onclick="plusSlides(1)">&#10095;</a>
+      <div class="slideshow-dots">
+        <?php
+          for($i = 0; $i < $postscount; $i++) {
+            echo('<span class="dot" onclick="currentSlide(' . $i .')"></span>');
+          }
+
+        ?>
+      </div>
+  </div>
+  <script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
 </section>
       <?php
 
@@ -23,4 +63,6 @@
 </main>
 <?php
   get_footer();
+
+
 ?>
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -8,10 +8,13 @@
  */
 
 ?>
-	<?php the_post_thumbnail(
-		'post-thumbnail',
-		array(
-			'alt' => the_title_attribute(array('echo' => false)),
-			'class' => "slideshow"
-		)
-	); ?>
+<div class="slide fade">
+		<?php the_post_thumbnail(
+			'post-thumbnail',
+			array(
+				'alt' => the_title_attribute(array('echo' => false)),
+				'class' => "slideshow"
+			)
+		); ?>
+    <span>Caption</span>
+</div>
